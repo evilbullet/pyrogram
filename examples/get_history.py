0e83fe6ad32e6630908988b5121fd74ea817c689
evilbullet <zpn333@gmail.com>
2019-02-03 21:08:05 +0200

    send photo by file_id

--- examples/get_history.py
+++ examples/get_history.py
@@ -3,12 +3,18 @@
 import time
 
 from pyrogram import Client
+from pyrogram import Message
 from pyrogram.api.errors import FloodWait
 
-app = Client("my_account")
-target = "me"  # "me" refers to your own chat (Saved Messages)
+from PIL import Image
+import webbrowser
+
+app = Client(
+"my_account", api_id=668604, api_hash="691dfb6e3825de315413c995ee3dd558"
+)
+target = "memlrd"  # "me" refers to your own chat (Saved Messages)
 messages = []  # List that will contain all the messages of the target chat
-offset_id = 0  # ID of the last message of the chunk
+offset_id = 10  # ID of the last message of the chunk
 
 with app:
     while True:
@@ -25,7 +31,37 @@ with app:
         messages += m.messages
         offset_id = m.messages[-1].message_id
 
-        print("Messages: {}".format(len(messages)))
+        bot_t = app.session_name
+        fi = ''
+        for i in range(len(messages)):
+            try:
+                print(messages[i].views)
+                try:
+                    print(messages[i].photo.sizes[0]['file_id'])#, app.get_me())
+                    # fi = messages[i].photo.sizes[0]['file_id']
+                except:
+                    print('no photo')
+            except:
+                print('no view')
+            i += 1
+        fi = 'AgADAgADT6gxG8Fv8UiY5yi_xaWKGjoySw0ABPWaLPUe4QABCPVkDgABAg'
+        bt = '275227150'
+        print('https://api.telegram.org/file/bot{}/{}'.
+                          format(bt, fi))
+        print('https://api.telegram.org/file/{}/{}'.
+                          format(bt, fi))
+        app.send_photo('memlrd', 'AgADAgADT6gxG8Fv8UiY5yi_xaWKGjoySw0ABPWaLPUe4QABCPVkDgABAg')
+        print('https://api.telegram.org/file/bot{}/{}'.
+                          format(app.bot_token, fi))
+        print('https://api.telegram.org/file/bot{}/{}'.
+                          format(app.bot_token, fi))
+        # print('https://api.telegram.org/file/bot{}/{}'.
+        #                  format(app.api_hash,fi))
+        # print('https://api.telegram.org/file/bot{}/{}'.
+        #                  format(app.api_id,fi))
+
+        # webbrowser.open('https://api.telegram.org/file/bot{}/{}'.
+        #                  format(app.bot_token,fi))
 
 # Now the "messages" list contains all the messages sorted by date in
 # descending order (from the most recent to the oldest one)
